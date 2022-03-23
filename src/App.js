d0e268a0a0b0ce2bf9541fbc246071005fe0c1c0
WeTop-69 <info@wetop.ru>
2022-03-23 19:21:45 +0300

    2

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
     <div className="App">
        <BrowserRouter>
         <Routes>
-          <Route  path="/login" element={<Login/>} />
-          <Route path="/firebase-taxi-app" element={<Main/>} />
-          <Route path="/firebase-taxi-app-info" element={<InfoCar/>} />
+          <Route path="site-frantsuz-go/y" element={<Login/>} />
+          <Route path="site-frantsuz-go/firebase-taxi-app" element={<Main/>} />
+          <Route path="site-frantsuz-go/firebase-taxi-app-info" element={<InfoCar/>} />
         </Routes>
       </BrowserRouter>
     </div>
