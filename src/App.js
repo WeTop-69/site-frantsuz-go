80f0c86adff822218757af27389c94fe07f6e585
WeTop-69 <102224839+WeTop-69@users.noreply.github.com>
2022-03-23 17:22:43 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component} from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import InfoCar from './componens/infoCar';
+import Login from './componens/login';
+import Main from './componens/main';
+
+function App() {
+  return (
+    <div className="App">
+       <BrowserRouter>
+        <Routes>
+          <Route index element={<Login/>} />
+          <Route path="firebase-taxi-app" element={<Main/>} />
+          <Route path="firebase-taxi-app-info" element={<InfoCar/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
